b5f72cd191d5afa2f952a64f8e4ba76e7cdd87a1
AbbyAdj <abbyadjei9@gmail.com>
2024-12-08 13:06:56 +0000

    New html files were created. Modules and their classes were edited. This commit was only pushed because I'm not going to have access to my local repository when I work on them next.

--- routes.py
+++ routes.py
@@ -1,8 +1,36 @@
 from flask import Blueprint, render_template
 
+# check https://startbootstrap.com/templates/general?showPro=false
+
 all_routes = Blueprint("routes", __name__)
 
-@all_routes.route("/")
+@all_routes.route("/login")
+def login():
+    return render_template("login.html")
+
+@all_routes.route("/account/<user>")
+def user_info():
+    return render_template("user_info.html")
+
+@all_routes.route("/home")
 def homepage():
-    return render_template("/templates/homepage.html")
+    return render_template("homepage.html")
+
+all_routes.route("/city/<city: str>")
+def city_information(city):
+    return render_template("city_information.html", city=city)
+
+all_routes.route("/travel/form")
+def input_form():
+    return render_template("input_form.html")
+
+all_routes.route("/travel/dice_roll")
+def dice_result():
+    return render_template("dice.html")
+
+all_routes.route("/travel/results")
+def travel_result():
+    return render_template("spontaneous_result.html")
+
+
 
